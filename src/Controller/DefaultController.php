bbb5298c205fb5ae59602cfe08c0e97406f7e356
TracyK-12 <tracy.marie149@gmail.com>
2025-04-25 15:18:26 +0200

    Finalisation projet symfony Atelier 1

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AnnonceRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'default_home', methods: ['GET'])]
+    public function home(AnnonceRepository $annonceRepository): Response
+    {
+        # Récupérer les derniers annonces les plus récentes
+         $annonces = $annonceRepository->findBy([], ['createdAt' => 'DESC']);
+        
+        
+        return $this->render('default/home.html.twig', [
+            'annonces' => $annonces,
+        ]);
+    }
+   
+}
